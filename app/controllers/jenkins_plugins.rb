c7d9465cbeb8d65a5279f8e1c239b725d8f5d728
sue <sue@netreqs.co.jp>
2011-10-17 11:06:12 +0900

    init

---
+++ app/controllers/jenkins_plugins.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'json'
+JenkinsPluginViewer.controllers :jenkins_plugins do
+
+  get :show do
+    json = ''
+    open('http://mirror.xmission.com/jenkins/updates/update-center.json') do |f|
+      json = f.read
+    end
+    json = json.sub('updateCenter.post(', '').sub(/\);$/, '')
+    @plugins = JSON.parse(json)
+    render 'jenkins_plugins/show'
+  end
+
+end

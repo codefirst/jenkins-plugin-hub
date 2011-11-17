2fbda6b71f238212d04250af2cb9040c0a372fb6
mallowlabs <mallowlabs@gmail.com>
2011-11-18 00:59:52 +0900

    Added: categories filter

--- app/controllers/jenkins_plugins.rb
+++ app/controllers/jenkins_plugins.rb
@@ -5,11 +5,34 @@ JenkinsPluginHub.controllers :jenkins_plugins do
   get :show, :map => '/' do
     json = ''
     open('http://mirror.xmission.com/jenkins/updates/update-center.json') do |f|
+    #open(File.dirname(__FILE__) + '/../../tmp/update-center.json') do |f|
       json = f.read
     end
     json = json.sub('updateCenter.post(', '').sub(/\);$/, '')
     @plugins = JSON.parse(json)
+    @categories = [
+      "scm",
+      "misc",
+      "notifier",
+      "listview-column",
+      "builder",
+      "user",
+      "ui",
+      "report",
+      "maven",
+      "buildwrapper",
+      "post-build",
+      "upload",
+      "external",
+      "trigger",
+      "page-decorator",
+      "slaves",
+      "scm-related",
+      "cluster",
+      "cli",
+      "envfile",
+      "must-be-labeled"
+    ]
     render 'jenkins_plugins/show'
   end
-
 end
